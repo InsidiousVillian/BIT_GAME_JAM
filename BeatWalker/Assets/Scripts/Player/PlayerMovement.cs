26af6ee8f79605c8f44163d9aa3c326e1c47d543
Jet Kingston <jettaikingston10@gmail.com>
2025-04-02 19:45:25 +0200

    Player now collides with trigger
    this will be changed later to create a new running section

--- BeatWalker/Assets/Scripts/Player/PlayerMovement.cs
+++ BeatWalker/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    public Rigidbody2D body;
+   
     void Start()
     {
-        
+        //allows us to control physics of gameobject - in fixed update we will use this to move player
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        body.linearVelocity = new Vector2(speed, body.linearVelocity.x);
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+{
+    Debug.Log("Collided with: " + other.name); // This checks if anything is detected
+
+    if (other.CompareTag("trigger"))
+    {
+        Debug.Log("Trigger has been triggered!");
+    }
+}
 }
